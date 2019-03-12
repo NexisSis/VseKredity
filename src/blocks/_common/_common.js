e6fbdbda69446009e1c162a66d632e1d050670d8
hyrdbyrd <IRuss23@yandex.ru>
2019-03-12 10:52:31 +0300

    feat

--- src/blocks/_common/_common.js
+++ src/blocks/_common/_common.js
@@ -1,3 +1,7 @@
 function updateValByName(val,inputName) {
     $("input[name=" + inputName + "]").val(val);
 }
+
+$(".accordeon__item").on("click", function () {
+    this.classList.toggle("accordeon__item_active");
+});
